d0027f5627cefff235658d5b742cea8a334407a8
Mat Sadler <mat@redstudio.co.uk>
2012-09-04 10:40:16 +0000

    gitignore works now

--- application/logs/log-2012-09-04.php
+++
@@ -1,5 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2012-09-04 10:32:52 --> 404 Page Not Found --> favicon.ico
-ERROR - 2012-09-04 10:32:56 --> 404 Page Not Found --> favicon.ico
-ERROR - 2012-09-04 10:33:04 --> 404 Page Not Found --> favicon.ico
