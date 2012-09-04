efe8e2ae99a2f9c264ed2d8e1e55e6b2fcbe0bce
Mat Sadler <mat@redstudio.co.uk>
2012-09-04 14:50:17 +0000

    added opening times

--- application/views/sidebox/main.php
+++ application/views/sidebox/main.php
@@ -4,3 +4,6 @@
    <a href="http://booking.clubwoodham.co.uk">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
     
 </div>
+<div id="sidebox" style="text-align:center" >
+<img src="https://s3-eu-west-1.amazonaws.com/clubwoodham/OpeningTimes.png" />
+</div>
