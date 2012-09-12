ba2d65eae4b86702d29a2df9d7ad54035334aa84
Mat Sadler <mat@redstudio.co.uk>
2012-09-12 13:16:24 +0000

    added https to booking link, and added login.aspx

--- application/views/sidebox/main.php
+++ application/views/sidebox/main.php
@@ -1,7 +1,7 @@
 <div id="sidebox" >
     <a href="<?=base_url()?>news"><img  src="<?= base_url() ?>images/icons/buttons/UpcomingEvents_Button.png"/></a>
-    <a href="http://booking.clubwoodham.co.uk"> <img  src="<?= base_url() ?>images/icons/buttons/BookACourt_Button.png"/></a>
-   <a href="http://booking.clubwoodham.co.uk">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
+    <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/BookACourt_Button.png"/></a>
+   <a href="https://booking.clubwoodham.co.uk/login.aspx">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
     
 </div>
 <div id="sidebox" style="text-align:center" >
