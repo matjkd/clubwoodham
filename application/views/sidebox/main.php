9adc6b1da2528e489d08f72fe1d5482043ae8fef
Mat Sadler <mat@redstudio.co.uk>
2013-09-12 12:46:05 +0100

    added member login

--- application/views/sidebox/main.php
+++ application/views/sidebox/main.php
@@ -1,8 +1,9 @@
 <div id="sidebox" >
+	  <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/memberloginbutton.png"/></a>
     <a href="<?=base_url()?>news"><img  src="<?= base_url() ?>images/icons/buttons/UpcomingEvents_Button.png"/></a>
    
-  <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/BookACourt_Button.png"/></a>
-   <a href="https://booking.clubwoodham.co.uk/login.aspx">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
+    <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/BookACourt_Button.png"/></a>
+   <a href="https://booking.clubwoodham.co.uk/Join/join_subscription.aspx">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
     
 </div>
 <div id="sidebox" style="text-align:center" >
