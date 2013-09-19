7cc354bb188a07a13ca8170e1b732fc48310b298
Mat Sadler <mat@redstudio.co.uk>
2013-09-19 14:09:44 +0100

    moved member login button to bottom

--- application/views/sidebox/main.php
+++ application/views/sidebox/main.php
@@ -1,10 +1,10 @@
 <div id="sidebox" >
-	  <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/memberloginbutton.png"/></a>
+	 
     <a href="<?=base_url()?>news"><img  src="<?= base_url() ?>images/icons/buttons/UpcomingEvents_Button.png"/></a>
    
     <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/BookACourt_Button.png"/></a>
    <a href="https://booking.clubwoodham.co.uk/Join/join_subscription.aspx">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
-    
+     <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/memberloginbutton.png"/></a>
 </div>
 <div id="sidebox" style="text-align:center" >
 <img src="https://s3-eu-west-1.amazonaws.com/clubwoodham/OpeningTimes.png" />
