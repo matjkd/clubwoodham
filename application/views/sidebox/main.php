82e042a1c9fadb60a6ad7b767d60821c219153e5
Mat Sadler <mat@redstudio.co.uk>
2016-01-02 22:43:42 +0000

    change links

--- application/views/sidebox/main.php
+++ application/views/sidebox/main.php
@@ -2,9 +2,9 @@
 	 
     <a href="<?=base_url()?>news"><img  src="<?= base_url() ?>images/icons/buttons/UpcomingEvents_Button.png"/></a>
    
-    <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/BookACourt_Button.png"/></a>
-   <a href="https://booking.clubwoodham.co.uk/Join/join_subscription.aspx">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
-     <a href="https://booking.clubwoodham.co.uk/login.aspx"> <img  src="<?= base_url() ?>images/icons/buttons/memberloginbutton.png"/></a>
+    <a href="https://ma.clubwise.com/clubwoodham/#"> <img  src="<?= base_url() ?>images/icons/buttons/BookACourt_Button.png"/></a>
+   <a href="https://ma.clubwise.com/clubwoodham/#">  <img  src="<?= base_url() ?>images/icons/buttons/JoinToday_Button.png"/></a>
+     <a href="https://ma.clubwise.com/clubwoodham/#"> <img  src="<?= base_url() ?>images/icons/buttons/memberloginbutton.png"/></a>
 </div>
 <div id="sidebox" style="text-align:center" >
 <img src="https://s3-eu-west-1.amazonaws.com/clubwoodham/OpeningTimes.png" />
